147ea638a870699bd4c10f6f277b39a51fd90813
Markus Andresen <markus.andresen94@gmail.com>
2019-03-23 19:51:13 +0100

    comments

--- src/redux/applyMiddleware.js
+++ src/redux/applyMiddleware.js
@@ -1,5 +1,13 @@
 const compose = require("./compose.js");
 
+/**
+ * Wraps stores dispatch function
+ *
+ * @param {Function} Middlewares to be applied.
+ *
+ * @returns {Object} New store object with a wrapped dispatch.
+ *
+ */
 const applyMiddleware = (...middlewares) => {
   return next => (reducer, initialState) => {
     const store = next(reducer, initialState);
