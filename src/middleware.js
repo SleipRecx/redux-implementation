c0f29fca06c93902073d5b0399a0ab94b35b390d
Markus Andresen <markus.andresen94@gmail.com>
2019-04-01 16:08:39 +0200

    Thunk and Logger middleware implemented

--- src/middleware.js
+++ src/middleware.js
@@ -1,14 +1,35 @@
 const isFSA = require("flux-standard-action").isFSA;
 
-const ensureFSA = _ => next => action => {
-  if (process.env.NODE_ENV !== "production") {
-    if (!isFSA(action)) {
-      throw new Error(
-        "Dispatched action is not a flux standard action" +
-          "See https://github.com/redux-utilities/flux-standard-action for more details."
-      );
-    }
+const ensureFSA = () => next => action => {
+  if (!isFSA(action)) {
+    throw new Error(
+      "Dispatched action is not a flux standard action" +
+        "See https://github.com/redux-utilities/flux-standard-action for more details."
+    );
   }
   next(action);
 };
-module.exports = ensureFSA;
+
+const thunk = ({ dispatch, getState }) => next => action => {
+  if (typeof action === "function") {
+    return action(dispatch, getState);
+  }
+  return next(action);
+};
+
+const logger = ({ getState }) => next => action => {
+  console.log("Previous State:");
+  console.log(getState());
+  console.log("Action:");
+  console.log(action);
+  const returnValue = next(action);
+  console.log("Next State:");
+  console.log(getState());
+  return returnValue;
+};
+
+module.exports = {
+  ensureFSA,
+  thunk,
+  logger
+};
