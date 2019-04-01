9670663eadc56a60c8beb70662a6cdb0da8db572
Markus Andresen <markus.andresen94@gmail.com>
2019-04-01 16:08:57 +0200

    Async increment action

--- index.js
+++ index.js
@@ -13,8 +13,21 @@ const decrement = require("./src/actionCreators").decrement;
 const addTodo = require("./src/actionCreators").addTodo;
 const toggleTodo = require("./src/actionCreators").toggleTodo;
 
+
+const asyncIncrement = () => {
+  return dispatch => {
+    setTimeout(() => {
+      dispatch(increment())
+    },1000)
+  }
+}
+
 // Import middleware
-const ensureFSA = require("./src/middleware")
+const ensureFSA = require("./src/middleware").ensureFSA
+const thunk = require("./src/middleware").thunk
+const logger = require("./src/middleware").logger
+
+
 
 // Combine Reducers
 const rootReducer = combineReducers({
@@ -22,22 +35,13 @@ const rootReducer = combineReducers({
   todos
 });
 
-
 // Create store and apply middleware
-const store = createStore(rootReducer, {}, applyMiddleware(ensureFSA));
-//console.log(store.dispatch.toString())
+const store = createStore(rootReducer, {}, applyMiddleware(thunk, logger, ensureFSA));
 
 // Subscribe to store changes
-const logCurrentStateTree = () => console.log(store.getState());
-const unsubscribe = store.subscribe(logCurrentStateTree);
+// const unsubscribe = store.subscribe(() => console.log(store.getState()));
 
 // Dispatch actions
-store.dispatch(increment());
-store.dispatch(decrement());
-store.dispatch(addTodo("Clean apartment"));
-const id = store.getState().todos[0].id;
-store.dispatch(toggleTodo(id));
-store.dispatch(toggleTodo(id));
-
-// Unsubscribe from store chngaes
-unsubscribe();
+store.dispatch(asyncIncrement());
+// Unsubscribe from store changes
+// unsubscribe();
